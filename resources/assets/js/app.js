152752da1a5e8c54e5b9361fed954d842c5b1b6d
Sudip Limbu <sudiplimburp@gmail.com>
2017-11-28 08:40:09 +0545

    created react preset and run:  npm install to install the dependencies

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,58 +1,16 @@
 
 /**
  * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
+ * includes React and other helpers. It's a great starting point while
+ * building robust, powerful web applications using React + Laravel.
  */
 
 require('./bootstrap');
 
-window.Vue = require('vue');
-
 /**
- * Global flash vue component
+ * Next, we will create a fresh React component instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-
-window.events = new Vue();
-
-window.flash = function (message) {
-    window.events.$emit('flash', message);
-};
-
-Vue.component('flash', require('./components/Flash.vue'));
-Vue.component('autocomplete', require('./components/Autocomplete.vue'));
-Vue.component('autoedit', require('./components/Autoedit.vue'));
-
-const app = new Vue({
-    el: '#app',
-    data: {
-        selected: 'marketing'
-
-    }
-});
-
-
-
-
-// Add tooltips
-$(function () {
-
-    $('[data-toggle="tooltip"]').tooltip()
-
-});
-
-// @admin Show hide inputs Note: To be moved to admin script later and use Vue instead of jQuery
-$(document).ready(function(){
-
-    $('.show-more').click(function(){
-        $('.more-inputs').toggle();
-    });
-/*
-    $('.carousel').carousel({
-
-        interval: false
-
-    });*/
-
-});
+require('./components/Example');
