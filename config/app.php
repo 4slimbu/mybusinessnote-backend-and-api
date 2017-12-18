bd30d4632ac39c363f39e1aa8778db6d004ca019
Sudip Limbu <sudiplimburp@gmail.com>
2017-12-18 13:35:00 +0545

    added  tymon/jwt-auth

--- config/app.php
+++ config/app.php
@@ -181,8 +181,8 @@ return [
          * Custom Service Providers
          */
         App\Providers\HelperClassServiceProvider::class,
-
         Collective\Html\HtmlServiceProvider::class,
+        Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class,
 
     ],
 
@@ -242,6 +242,9 @@ return [
 
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class
     ],
 
 ];
