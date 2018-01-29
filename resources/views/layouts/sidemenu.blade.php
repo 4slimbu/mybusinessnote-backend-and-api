907b04c8a7d292aa4170488705ec79d46d46a542
Ramesh <ramesh@octomedia.com.au>
2018-01-29 17:13:21 +1100

    removes dropdown from  top header My Account, moves Logout to left sidebar menu

--- resources/views/layouts/sidemenu.blade.php
+++ resources/views/layouts/sidemenu.blade.php
@@ -19,6 +19,17 @@
               Change Password
             </a>
           </li>
+          <li class="nav-item">
+          <a class="nav-link" href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+                       document.getElementById('logout-form').submit();">
+              <i class="fa fa-sign-out" aria-hidden="true"></i> Logout
+          </a>
+
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              {{ csrf_field() }}
+          </form>
+          </li>
          
         </ul>
     </div>
