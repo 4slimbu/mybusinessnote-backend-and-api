3536653d3ad28611b4f4e450371edcb11076e7b9
Ramesh <ramesh@octomedia.com.au>
2018-01-29 14:37:11 +1100

    adds favicon, updates bootstrap to version 4 from beta 4, improved user dashboard UI

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,43 +5,12 @@
 
         @include('layouts.sidemenu')
 
-        <main class="col-sm-9 ml-sm-auto col-md-10 pt-3" role="main">
-            <h1>User Dashboard</h1>
+        <main class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4" role="main">
+            
 
-            <h2>View User Businesses</h2>
+            <h2>Your Business Profile</h2>
 
-            <div class="table-responsive">
-                <table class="table table-striped">
-                    <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>Business Name</th>
-                        <th>Business E-Mail</th>
-                        <th>Badge Image</th>
-                        <th>Actions</th>
-
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($business as $bus)
-                        <tr>
-                            <td>{{ $bus->id }}</td>
-                            <td>{{ $bus->business_name }} </td>
-                            <td>{{ $bus->business_email }}</td>
-                            <td><img src="{{url('images/badges/')}}/{{$bus->badge->icon}}" alt="{{$bus->badge->name}}" width="150px" height="150px"></td>
-                            
-                            <td>
-
-                                <div class="">
-                                    <a href="{{url('business/edit')}}/{{$bus->id}}" class="btn btn-primary">Edit</a>
-                                     
-                        
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
+         
 
         </main>
     </div>
--- resources/views/layouts/sidemenu.blade.php
+++ resources/views/layouts/sidemenu.blade.php
@@ -1,44 +1,26 @@
-<nav class="col-sm-3 col-md-2 d-none d-sm-block admin-sidebar sidebar">
-    <div id="accordion" role="tablist">
-
-        <div class="card">
-            <div class="card-header" role="tab" id="headingTwo">
-                <h5 class="mb-0">
-                    <a class="collapsed" data-toggle="collapse" href="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                       Dash Board
-                    </a>
-                </h5>
-            </div>
-            <div id="collapseTwo" class="collapse show" role="tabpanel" aria-labelledby="headingTwo" data-parent="#accordion">
-                <div class="card-body">
-
-
-                    <ul class="nav nav-pills flex-column">
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{url('home')}}">My Businesses</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Communication Preferences</a>
-                        </li>
-
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{url('password-change')}}">
-                                Password change
-                            </a>
-                        </li>                
-
-                    </ul>
-                </div>
-            </div>
-        </div>
-        
+<nav class="col-md-2 d-none d-md-block bg-light sidebar">
+    <div class="sidebar-sticky">
+        <ul class="nav flex-column">
+          <li class="nav-item">
+            <a class="nav-link active" href="{{url('home')}}">
+              <i class="fa fa-briefcase" aria-hidden="true"></i>
+              My Business Profile <span class="sr-only">(current)</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{url('user-preferences')}}">
+              <i class="fa fa-envelope-o" aria-hidden="true"></i>
+              Communication Preferences
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="{{url('password-change')}}">
+            <i class="fa fa-lock" aria-hidden="true"></i>
+              Change Password
+            </a>
+          </li>
+         
+        </ul>
     </div>
-
-
-
-
-
 </nav>
 
