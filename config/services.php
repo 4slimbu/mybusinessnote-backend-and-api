12d94d341d5909dce6dadefd59f82e47276558bd
Sudip Limbu <4slimbu@gmail.com>
2018-02-13 08:33:16 +0545

    Update redirect url for social logins

--- config/services.php
+++ config/services.php
@@ -38,13 +38,13 @@ return [
     'google' => [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-        'redirect' =>  '/start/login/oauth/google/callback',
+        'redirect' =>  env('GOOGLE_REDIRECT_URL'),
     ],
 
     'facebook' => [
         'client_id' => env('FACEBOOK_CLIENT_ID'),
         'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
-        'redirect' =>  '/start/login/oauth/facebook/callback',
+        'redirect' =>  env('FACEBOOK_REDIRECT_URL'),
     ]
 
 ];
