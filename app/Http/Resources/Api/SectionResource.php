89f94cb60ae1907cbb618fe09b22084ff3a9275b
Sudip Limbu <sudiplimburp@gmail.com>
2018-03-09 14:18:44 +0545

    Refactoring

--- app/Http/Resources/Api/SectionResource.php
+++ app/Http/Resources/Api/SectionResource.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Resources\Api;
 
+use App\Traits\Authenticable;
 use Illuminate\Http\Resources\Json\Resource;
 
 class SectionResource extends Resource
 {
+    use Authenticable;
     /**
      * Transform the resource into an array.
      * @param \Illuminate\Http\Request $request
@@ -13,13 +15,26 @@ class SectionResource extends Resource
      */
     public function toArray($request)
     {
+        try {
+            $user =  $this->getAuthUser();
+        } catch (\Exception $exception) {
+            $user = null;
+        }
+
+        if ($user) {
+            $businessOptions = $user->business->businessOptions()->where('section_id', $this->id)->pluck('id');
+        } else {
+            $businessOptions = $this->businessOptions->pluck('id');
+        }
+
         return [
             'id' => $this->id,
             'level_id' => $this->level_id,
             'name' => $this->name,
             'slug' => $this->slug,
             'icon' => asset($this->uploadDirectory . $this->icon),
-            'tooltip' => $this->tooltip
+            'tooltip' => $this->tooltip,
+            'businessOptions' => $businessOptions
         ];
     }
 }
