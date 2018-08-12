e0b24b4322876273dfeb7cb957ef7ad48f29e9cd
Sudip Limbu <responsivesudip@gmail.com>
2018-08-12 16:12:18 +0545

    Depricate short_name field on business option

--- resources/views/admin/business-option/includes/form.blade.php
+++ resources/views/admin/business-option/includes/form.blade.php
@@ -10,8 +10,8 @@
 {{--Short Name--}}
 <div class="form-group">
     <label>Short Name:</label>
-    <div><small>This name will be used on modal box layout where there is less space</small></div>
-    {{ Form::text('short_name', null, ['class' => 'form-control']) }}
+    <div><small>This field is no longer used and will be removed on next release. Only the name field is used from now on.</small></div>
+    {{ Form::text('short_name', null, ['class' => 'form-control', 'disabled' => 'disabled']) }}
     @if($errors->has('short_name'))
         <span class="text-danger">{{ $errors->first('short_name') }}</span>
     @endif
@@ -39,6 +39,11 @@
 {{--Section--}}
 <div class="form-group">
     <label class="display-block text-semibold">Section:</label>
+    <div>
+        <small>
+            The business option always belongs to a section.
+        </small>
+    </div>
     {{ Form::select('section_id', $data['sections'], null, ['class' => 'form-control', 'placeholder' => '-- Select Section --']) }}
     @if($errors->has('section_id'))
         <span class="text-danger">{{ $errors->first('section_id') }}</span>
@@ -48,6 +53,11 @@
 {{--Business Category--}}
 <div id="business_categories" class="form-group more-inputs">
     <label  class="display-block text-semibold">Select categories where this option should be visible:</label>
+    <div>
+        <small>
+            The business option should have at least one category selected.
+        </small>
+    </div>
     @if($data['businessCategories'])
         @foreach($data['businessCategories'] as $key => $item)
             <div class="row">
@@ -70,6 +80,12 @@
 {{--Parent Business Option--}}
 <div class="form-group">
     <label class="display-block text-semibold">Parent Business Option:</label>
+    <div>
+        <small>
+            If you want to nest the business option as child of another business option, then choose this otherwise
+            you can left it blank.
+        </small>
+    </div>
     {{ Form::select('parent_id', $data['businessOptions'], null, ['placeholder' => '-- Choose Parent Busines Option --', 'class' => 'form-control']) }}
     @if($errors->has('parent_id'))
         <span class="text-danger">{{ $errors->first('parent_id') }}</span>
@@ -139,36 +155,43 @@
 </div>
 
 {{--Tool Tip--}}
-<div class="form-group">
-    <label>Tool Tip:</label>
-    {{ Form::textarea('tooltip', null, ['class' => 'form-control myeditablediv']) }}
-    @if($errors->has('tooltip'))
-        <span class="text-danger">{{ $errors->first('tooltip') }}</span>
-    @endif
-</div>
+            <div class="form-group">
+                <label>Tool Tip:</label>
+                {{ Form::textarea('tooltip', null, ['class' => 'form-control myeditablediv']) }}
+                @if($errors->has('tooltip'))
+                    <span class="text-danger">{{ $errors->first('tooltip') }}</span>
+                @endif
+            </div>
 
-{{--Affiliate Link Label--}}
-<div class="form-group">
-    <label>Affiliate Link Label:</label>
-    {{ Form::text('affiliate_link_label', $data['selectedAffiliateLinkLabel'], ['class' => 'form-control']) }}
-    @if($errors->has('affiliate_link_label'))
-        <span class="text-danger">{{ $errors->first('affiliate_link_label') }}</span>
-    @endif
-</div>
+            {{--Affiliate Link Label--}}
+            {{--<div class="form-group">--}}
+            <label>Affiliate Link Label:</label>
+            {{ Form::text('affiliate_link_label', $data['selectedAffiliateLinkLabel'], ['class' => 'form-control']) }}
+            @if($errors->has('affiliate_link_label'))
+                <span class="text-danger">{{ $errors->first('affiliate_link_label') }}</span>
+        @endif
+    </div>
 
-{{--Partner Affiliate Links--}}
-<div class="form-group">
-    <label class="display-block text-semibold">Affiliate Links:</label>
-    {{ Form::select('affiliate_link_id[]', $data['affiliateLinks'], $data['selectedAffiliateLinks'], ['placeholder' => '-- Choose Affiliate Links --', 'class' => 'form-control']) }}
-    @if($errors->has('affiliate_link_id'))
-        <span class="text-danger">{{ $errors->first('affiliate_link_id') }}</span>
-    @endif
-</div>
+    {{--Partner Affiliate Links--}}
+    <div class="form-group">
+        <label class="display-block text-semibold">Affiliate Links:</label>
+        {{ Form::select('affiliate_link_id[]', $data['affiliateLinks'], $data['selectedAffiliateLinks'], ['placeholder' => '-- Choose Affiliate Links --', 'class' => 'form-control']) }}
+        @if($errors->has('affiliate_link_id'))
+            <span class="text-danger">{{ $errors->first('affiliate_link_id') }}</span>
+        @endif
+    </div>
 
 
-{{--Weight--}}
-<div class="form-group">
-    <label>Weight:</label>
+    {{--Weight--}}
+    <div class="form-group">
+        <label>Weight:</label>
+        <div>
+            <small>
+                Weight will determine by how much amount should the percent be increased when this particular business option is completed.
+                It is relative and for almost all cases it's value should be 1. If there are 4 business options in a section, then putting weight = 1
+                on all business options will increase the completed percent by 25% when any of the business option is completed. But, if you put weight = 1 in 3 of them and
+                on one weight = 2. Then first three will increase the completed percent by 20% and the last one will increase the completed percent by 40%.
+        </small></div>
     {{ Form::text('weight', null, ['class' => 'form-control']) }}
     @if($errors->has('weight'))
         <span class="text-danger">{{ $errors->first('weight') }}</span>
@@ -178,6 +201,12 @@
 {{--Template--}}
 <div class="form-group">
     <label class="display-block text-semibold">Template:</label>
+    <div>
+        <small>
+            There are two type of layouts in our business option pages. The level one has full width type of style where as
+            other levels have boxed type of style. So, choose 'default' for level one and choose 'modal box' for other levels.
+        </small>
+    </div>
     {{ Form::select('template', ['default' => 'Default', 'modal_box' => 'Modal Box'], null, ['class' => 'form-control']) }}
     @if($errors->has('template'))
         <span class="text-danger">{{ $errors->first('template') }}</span>
