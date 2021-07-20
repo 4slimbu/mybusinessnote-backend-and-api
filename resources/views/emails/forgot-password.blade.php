5e1829b3b3d7a78697e4cfc0b37fc63cef6feebc
Sudip Limbu <4slimbu@gmail.com>
2021-07-20 16:56:14 +0000

    Update email templates

--- resources/views/emails/forgot-password.blade.php
+++ resources/views/emails/forgot-password.blade.php
@@ -1,7 +1,8 @@
 @component('mail::message')
 Hello {{ $data['Data']['first_name'] }},
 
-Please click the link below to reset your password.
+A password reset request has been made to this email. If you are not the one who made this 
+request, then ignore this email. Otherwise, follow this link to reset your password.
 
 
 @component('mail::button', ['url' => $data['Data']['reset_password_link']])
@@ -9,5 +10,5 @@ Please click the link below to reset your password.
 @endcomponent
 
 Thanks,<br>
-{{ config('app.name') }}
+{{ config('app.name') }} Team
 @endcomponent
--- resources/views/emails/verification.blade.php
+++ resources/views/emails/verification.blade.php
@@ -1,13 +1,13 @@
 @component('mail::message')
 Hello {{ $data['Data']['first_name'] }},
 
-The body of your message.
-
+Welcome to {{ config('app.name') }}. 
+You are just a step away from taking note about your business journey. Please verify your email and make your journey smoother.
 
 @component('mail::button', ['url' => $data['Data']['continue_where_you_left']])
     Verify Now
 @endcomponent
 
 Thanks,<br>
-{{ config('app.name') }}
+{{ config('app.name') }} Team
 @endcomponent
